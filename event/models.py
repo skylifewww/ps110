a91d682984462c5325db2932e39e1cfc8c479588
Asaf Klibansky <discobeta@gmail.com>
2016-04-18 18:02:00 +0000

    Added a new class for parent

--- event/models.py
+++ event/models.py
@@ -8,6 +8,20 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Parent(models.Model):
+
+	name = models.CharField(max_length=200)
+	phone = models.CharField(max_length=200)
+	child_name = models.CharField(max_length=200)
+	classrooms = models.CharField(max_length=200)
+	created = models.DateTimeField(default=timezone.now())
+	def __str__(self):
+		return self.created
+
+
+
+
+
 class Activity(models.Model):
 
 	user = models.IntegerField(max_length=200)
