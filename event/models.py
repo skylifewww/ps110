32cf8c34a9d40c56976e80a686a3bfbf76f78f0e
Asaf Klibansky <discobeta@gmail.com>
2016-04-18 15:35:00 +0000

    Added a new class for activity

--- event/models.py
+++ event/models.py
@@ -2,4 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+from jsonfield import JSONField
+from django.utils import timezone
+
 # Create your models here.
+
+class Activity(models.Model):
+
+	user = models.IntegerField(max_length=200)
+	created = models.DateTimeField(default=timezone.now())
+	event = models.CharField(max_length=200)
+	def __str__(self):
+		return self.event
+
+
+
+
