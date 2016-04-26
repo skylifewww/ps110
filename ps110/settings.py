2519313ac4d523ebb27e366a8d34efc4b6e7b8bb
Asaf Klibansky <discobeta@gmail.com>
2016-04-26 13:11:00 +0000

    Added event to PS110 settings.py

--- ps110/settings.py
+++ ps110/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'event.apps.EventConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
