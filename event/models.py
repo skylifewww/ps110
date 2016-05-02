b83c23cd5027f2f72d819378f471039c55c1db13
Asaf Klibansky <discobeta@gmail.com>
2016-05-02 14:22:00 +0000

    Added Token schema for rest framework to use in models.py

--- event/models.py
+++ event/models.py
@@ -10,6 +10,13 @@ from jsonfield import JSONField
 from django.utils import timezone
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser, User
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
 
 
 
