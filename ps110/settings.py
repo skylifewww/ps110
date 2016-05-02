14fe6d0420181f9650f0648289df6973ebe78e04
Asaf Klibansky <discobeta@gmail.com>
2016-05-02 10:17:00 +0000

    Added rest_framework.password validation to PS110 settings.py

--- ps110/settings.py
+++ ps110/settings.py
@@ -148,6 +148,22 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+AUTH_PASSWORD_VALIDATORS = [
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
+]
+
+
 REST_FRAMEWORK = {
 	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 	'PAGE_SIZE': 100,
