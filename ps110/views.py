90fb391b189c7d5e16cdc5ef18f29aec31919d86
Asaf Klibansky <discobeta@gmail.com>
2016-05-09 14:42:00 +0000

    added function for user creation to views

--- ps110/views.py
+++ ps110/views.py
@@ -15,3 +15,27 @@ def home(request):
 
 	context = RequestContext(request, {'user': request.user})
 	return render_to_response('home.html', context_instance=context)
+
+@csrf_exempt
+def create_auth(request):
+	if request.POST.get('email') and request.POST.get('password'):
+		try:
+			user = User.objects.get(username=request.POST.get('email').replace(' ',''))
+		except User.DoesNotExist:
+			user = None
+		if user:
+			return HttpResponse('{"error":"username already exists"}', content_type="application/json")
+		else:
+			user = User.objects.create_user(
+				request.POST.get('email'),
+				request.POST.get('email'),
+				request.POST.get('password')
+			)
+			jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+			jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+			payload = jwt_payload_handler(user)
+			token = jwt_encode_handler(payload)
+			return HttpResponse(token, content_type="application/json")
+	else:
+		return HttpResponse('{"error":"missing parameters, could not create user"}', content_type="application/json")
+
